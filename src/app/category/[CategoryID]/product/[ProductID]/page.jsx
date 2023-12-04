8492e8de042f447d212780a8cfad95288d532c59
Ziyad Ashraf <ziyadazab36@gmail.com>
2023-12-04 14:03:09 +0200

    Constructed Single product

---
+++ src/app/category/[CategoryID]/product/[ProductID]/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+// import styles from './page.module.css'
+import Header from "../../../../../../components/Header/Header"
+import CategoriesAd from '../../../../../../components/Category/HeroAd/CategoriesAd'
+import Details from '../../../../../../components/Category/HeroAd/Details/Details'
+
+export default function Product() {
+    return (
+        <>
+
+            Hi from product Category
+        </>
+
+    )
+}
---
+++ src/app/category/[CategoryID]/subcategory/[SubcategoryID]/product/[ProductID]/page.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+// // import styles from './page.module.css'
+// import Header from "../../../../../../components/Header/Header"
+// import CategoriesAd from '../../../../../../components/Category/HeroAd/CategoriesAd'
+// import Details from '../../../../../../../../components/Category/HeroAd/Details/Details'
+
+export default function Product() {
+    return (
+        <>
+
+            hi from product subcategory
+
+        </>
+
+    )
+}
